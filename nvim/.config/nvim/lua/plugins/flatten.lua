9cb8cf90366b4c93861c2a3856047e66b54ebafe
EsdrasAmora <esdras.amora@gmail.com>
2023-10-14 18:29:38 -0300

    Update all, add back wezterm file hyperlinks and flatten nvim

---
+++ nvim/.config/nvim/lua/plugins/flatten.lua
@@ -0,0 +1,12 @@
+return {
+  "willothy/flatten.nvim",
+  config = true,
+  lazy = false,
+  priority = 1001,
+  opts = {
+    window = { open = "alternate" },
+    one_per = {
+      wezterm = true,
+    },
+  },
+}
