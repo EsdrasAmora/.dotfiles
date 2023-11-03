e8f8d0d1ecef825325cd8520c54acbaffdb08c36
EsdrasAmora <esdras.amora@gmail.com>
2023-11-02 22:19:52 -0300

    Bump deps and temp disable copilot

--- nvim/.config/nvim/lua/plugins/treesiter.lua
+++ nvim/.config/nvim/lua/plugins/treesiter.lua
@@ -1,6 +1,5 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "nvim-treesitter/playground",
-  },
+  { "nvim-treesitter/nvim-treesitter", dependencies = { "nvim-treesitter/playground" } },
+  -- { "abecodes/tabout.nvim" },
+  { "boltlessengineer/smart-tab.nvim" },
 }
